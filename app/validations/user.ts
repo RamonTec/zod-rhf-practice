8eea4479bbfb33f6459819cccf4e23dbe8481b6a
elias estrabao <eestrabao46@gmail.com>
2025-09-07 18:13:26 -0400

    feat: validations practice

--- app/validations/user.ts
+++ app/validations/user.ts
@@ -6,3 +6,11 @@ export const UserSchema = z.object({
   email: z.string(),
   age: z.number(),
 });
+
+// note email here is deprecated
+export const UserSchemaV2 = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters long"),
+  age: z.number().int().positive("Age must be a positive integer"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
